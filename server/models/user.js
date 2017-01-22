b12a830d6228d5f67b8570c9f064e68606e5be36
NickRbk <nrybak94@gmail.com>
2017-01-21 17:50:42 -0800

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -59,6 +59,23 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  let User = this;
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, 'secret_key');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 const port = process.env.PORT || 8080;
 
@@ -21,6 +22,12 @@ require('./routes/todosID-route')(app, Todo, ObjectID);
 require('./routes/todosID-delete-route')(app, Todo, ObjectID);
 require('./routes/todosID-patch-route')(app, Todo, ObjectID, _);
 
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
